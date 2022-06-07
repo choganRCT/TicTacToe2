1d314bf8fa3a319eb74838da168ae6aa96c2155c
Cory.Hogan@Regalcuttingtools.com <Cory.Hogan@Regalcuttingtools.com>
2022-06-07 14:26:35 -0500

    Add project files.

---
+++ TicTacToe2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TicTacToe2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new TicTacToeGame();
+            game.Play();
+        }
+    }
+}
---
+++ TicTacToe2/TicTacToeGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe2
+{
+    class TicTacToeGame
+    {
+        public void Play()
+        {
+
+        }
+    }
+}
