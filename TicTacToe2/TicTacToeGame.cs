56eac7a11c825c44e228066d34151fc3c0897f45
KAllen <kristine.allen@regalcuttingtools.com>
2022-06-16 17:07:23 -0500

    Close Change Request #1 on game baord

--- TicTacToe2/TicTacToeGame.cs
+++ TicTacToe2/TicTacToeGame.cs
@@ -6,7 +6,7 @@ namespace TicTacToe2
 {
     class TicTacToeGame
     {
-        static readonly string[] space = new string[9] {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        private readonly string[] space = new string[9] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
         public void Play()
         {
             bool winner = false;
@@ -14,7 +14,6 @@ namespace TicTacToe2
             while (!winner && !draw)
             {
                 DrawBoard();
-                Console.ReadLine();
                 GetUserInput();
                 draw = CheckForDraw();
                 winner = CheckForWin();
@@ -24,24 +23,30 @@ namespace TicTacToe2
 
         private void DrawBoard()
         {
-            Console.WriteLine($"\t {space[0]} | {space[1]} | {space[2]}");
+            Console.WriteLine($"\n\t {space[0]} | {space[1]} | {space[2]}");
             Console.WriteLine("\t-----------");
             Console.WriteLine($"\t {space[3]} | {space[4]} | {space[5]}");
             Console.WriteLine("\t-----------");
             Console.WriteLine($"\t {space[6]} | {space[7]} | {space[8]}");
         }
+
         private void GetUserInput()
         {
-            throw new NotImplementedException();
+            //string player = "X";
+            //string choiceText;
+            //bool validInput = false;
         }
+
         private bool CheckForDraw()
         {
             throw new NotImplementedException();
         }
+
         private bool CheckForWin()
         {
             throw new NotImplementedException();
         }
+
         private void ChangePlayer()
         {
             throw new NotImplementedException();
