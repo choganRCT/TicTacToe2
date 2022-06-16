64099d243aba66976bdc872fdd0bd0092c35b020
KAllen <kristine.allen@regalcuttingtools.com>
2022-06-16 15:42:47 -0500

    Update Game Play #1

--- TicTacToe2/TicTacToeGame.cs
+++ TicTacToe2/TicTacToeGame.cs
@@ -6,6 +6,7 @@ namespace TicTacToe2
 {
     class TicTacToeGame
     {
+        static readonly string[] space = new string[9] {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
         public void Play()
         {
             bool winner = false;
@@ -13,6 +14,7 @@ namespace TicTacToe2
             while (!winner && !draw)
             {
                 DrawBoard();
+                Console.ReadLine();
                 GetUserInput();
                 draw = CheckForDraw();
                 winner = CheckForWin();
@@ -22,7 +24,11 @@ namespace TicTacToe2
 
         private void DrawBoard()
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"\t {space[0]} | {space[1]} | {space[2]}");
+            Console.WriteLine("\t-----------");
+            Console.WriteLine($"\t {space[3]} | {space[4]} | {space[5]}");
+            Console.WriteLine("\t-----------");
+            Console.WriteLine($"\t {space[6]} | {space[7]} | {space[8]}");
         }
         private void GetUserInput()
         {
