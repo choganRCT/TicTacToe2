97221593e8885e789a6f52c6e4b90bbdd2d7783a
KAllen <kristine.allen@regalcuttingtools.com>
2022-06-20 15:24:30 -0500

    Created game play

--- TicTacToe2/TicTacToeGame.cs
+++ TicTacToe2/TicTacToeGame.cs
@@ -7,22 +7,26 @@ namespace TicTacToe2
     class TicTacToeGame
     {
         private readonly string[] space = new string[9] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        private string player = "X";
+        private int turnCount = 0;
+        private bool winner = false;
+        private bool draw = false;
+
         public void Play()
         {
-            bool winner = false;
-            bool draw = false;
             while (!winner && !draw)
             {
                 DrawBoard();
                 GetUserInput();
                 draw = CheckForDraw();
                 winner = CheckForWin();
-                ChangePlayer();
+                player = ChangePlayer();
             }
         }
 
         private void DrawBoard()
         {
+            Console.Clear();
             Console.WriteLine($"\n\t {space[0]} | {space[1]} | {space[2]}");
             Console.WriteLine("\t-----------");
             Console.WriteLine($"\t {space[3]} | {space[4]} | {space[5]}");
@@ -32,24 +36,142 @@ namespace TicTacToe2
 
         private void GetUserInput()
         {
-            //string player = "X";
-            //string choiceText;
-            //bool validInput = false;
+            string choiceText;
+            bool validInput = false;
+            bool legalMove = false;
+
+            while (!validInput && !legalMove)
+            {
+                try
+                {
+                    Console.WriteLine($"\nIt is player {player}'s turn.");
+                    Console.Write("Pick a number from the board to place your mark: ");
+                    choiceText = Console.ReadLine();
+
+                    bool isParsable = Int32.TryParse(choiceText, out int choice);
+                    if (isParsable)
+                    {
+                        if (choice <= 0 || choice > 9)
+                        {
+                            validInput = false;
+                            Console.WriteLine("Please enter a number between 1-9.");
+                        }
+                        else
+                        {
+                            choice--;
+                            if (space[choice] == "X" || space[choice] == "O")
+                            {
+                                choice++;
+                                Console.WriteLine($"\n{choice} is already taken.");
+                                legalMove = false;
+                            }
+                            else
+                            {
+                                space[choice] = player;
+                                legalMove = true;
+                                validInput = false;
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    validInput = true;
+                }
+                turnCount++;
+            }
         }
 
         private bool CheckForDraw()
         {
-            throw new NotImplementedException();
+            if (turnCount == 9)
+            {
+                Console.Clear();
+                DrawBoard();
+                Console.WriteLine("\nThis game ended in a draw.");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         private bool CheckForWin()
         {
-            throw new NotImplementedException();
+            if (space[0] == space[1] && space[1] == space[2])
+            {
+                Console.Clear();
+                DrawBoard();
+                Console.WriteLine($"\nCongratulations!! {player} won.");
+                return true;
+            }
+            else if (space[3] == space[4] && space[4] == space[5])
+            {
+                Console.Clear();
+                DrawBoard();
+                Console.WriteLine($"\nCongratulations!! {player} won.");
+                return true;
+            }
+            else if (space[6] == space[7] && space[7] == space[8])
+            {
+                Console.Clear();
+                DrawBoard();
+                Console.WriteLine($"\nCongratulations!! {player} won.");
+                return true;
+            }
+            else if (space[0] == space[3] && space[3] == space[6])
+            {
+                Console.Clear();
+                DrawBoard();
+                Console.WriteLine($"\nCongratulations!! {player} won.");
+                return true;
+            }
+            else if (space[1] == space[4] && space[4] == space[7])
+            {
+                Console.Clear();
+                DrawBoard();
+                Console.WriteLine($"\nCongratulations!! {player} won.");
+                return true;
+            }
+            else if (space[2] == space[5] && space[5] == space[8])
+            {
+                Console.Clear();
+                DrawBoard();
+                Console.WriteLine($"\nCongratulations!! {player} won.");
+                return true;
+            }
+            else if (space[0] == space[4] && space[4] == space[8])
+            {
+                Console.Clear();
+                DrawBoard();
+                Console.WriteLine($"\nCongratulations!! {player} won.");
+                return true;
+            }
+            else if (space[2] == space[4] && space[4] == space[6])
+            {
+                Console.Clear();
+                DrawBoard();
+                Console.WriteLine($"\nCongratulations!! {player} won.");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
-        private void ChangePlayer()
+        private string ChangePlayer()
         {
-            throw new NotImplementedException();
+            if (player == "X")
+            {
+                return "O";
+            }
+            else
+            {
+                return "X";
+            }
         }
     }
 }
+
