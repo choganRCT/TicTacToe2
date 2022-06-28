ddf8d26a2584a25c3227b8986414be4820440567
Cory.Hogan@Regalcuttingtools.com <Cory.Hogan@Regalcuttingtools.com>
2022-06-28 15:19:36 -0500

    added board, game, and player

---
+++ TicTacToe2/TicTacToe/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe2.TicTacToe
+{
+    class Game
+    {
+        public void Play()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
