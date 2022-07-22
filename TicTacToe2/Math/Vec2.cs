7441c31a12d19d2d36bbaee7117a6e916a6fa50b
Cory.Hogan@Regalcuttingtools.com <Cory.Hogan@Regalcuttingtools.com>
2022-07-22 15:56:39 -0500

    Added Vec2 class

---
+++ TicTacToe2/Math/Vec2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe2.Math
+{
+    class Vec2 // 2d vector
+    {
+        // Properties: X, Y (int). These properties should be read-only, meaning they should only have get, not set
+
+        // Constructor that takes x, y arguments
+        // Constructor that takes no arguments and sets X and Y to zero
+
+        // Methods to perform these operations:
+        // 1. Add int value x to the X value of the vector
+        // 2. Add int value y to the Y value of the vector
+        // 3. Add int values x and y to the vector
+        // 4. Add another Vec2 to the vector
+        // 5. Subtract int value x from X value of the vector
+        // 6. Subtract int value y from Y value of the vector
+        // 7. Subtract int values x and y from the vector
+        // 8. Subtract another Vec2 from the vector
+        // 9. Copy the vector
+
+        // This class is immutable - its values cannot change. The methods above will need to return a new vector that contains the results of the calculation
+    }
+}
