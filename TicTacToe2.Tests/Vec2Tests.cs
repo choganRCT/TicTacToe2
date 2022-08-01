4f57800d386b64e59755417214406c07395b73bb
Cory.Hogan@Regalcuttingtools.com <Cory.Hogan@Regalcuttingtools.com>
2022-08-01 09:27:45 -0500

    also check y value in AddX tests

--- TicTacToe2.Tests/Vec2Tests.cs
+++ TicTacToe2.Tests/Vec2Tests.cs
@@ -13,18 +13,21 @@ namespace TicTacToe2.Tests
                 var a = new Vec2(1, 1);
                 var b = a.AddX(1);
                 Assert.AreEqual(2, b.X);
+                Assert.AreEqual(1, b.Y);
             }
 
             {
                 var a = new Vec2(1, 1);
                 var b = a.AddX(-2);
                 Assert.AreEqual(-1, b.X);
+                Assert.AreEqual(1, b.Y);
             }
 
             {
                 var a = new Vec2(1, 1);
                 var b = a.AddX(0);
                 Assert.AreEqual(1, b.X);
+                Assert.AreEqual(1, b.Y);
             }
         }
 
